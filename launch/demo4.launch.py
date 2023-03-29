d50e02b3659d367e602f9b830c09d7b03e353f5d
zealzel <zealzel@gmail.com>
2023-03-29 09:48:10 +0800

    Add more comments to the demo4.launch.py file

--- launch/demo4.launch.py
+++ launch/demo4.launch.py
@@ -5,6 +5,8 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     """
+    Minimal demenstration of how DecalreLaunchArgument and LaunchConfiguration work.
+    ex:
     ros2 launch my_ros2_tutorials demo4.launch.py node_name:=changed_talker
     """
     node_name_arg = DeclareLaunchArgument("node_name", default_value="my_talker")
